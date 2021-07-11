33f816a30033a225bf9fa42b0eff6790deaf5a34
Jeff Thomassen <jeff.thomassen@ucdenver.edu>
2021-07-10 19:06:47 -0600

    Completed Site

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-function addingEventListener() {
-    const input = document.getElementById('input');
-
-    function clickAlert() {
-        alert('I was clicked!');
-    }
-
-input.addEventListener('click', clickAlert); 
+const form = document.getElementById('form')
+function formSubmit(){
+  alert('You are beautiful or handsome. And sexy :)')
 }
+form.addEventListener('submit', formSubmit)
